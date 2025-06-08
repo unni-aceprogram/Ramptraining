1d1f35bf8805b1b43d8984a5c700f2b977387cb3
unni-aceprogram <prestigeiris2021@gmail.com>
2025-06-09 05:21:05 +0530

    Add files via upload

--- scriptelegant.js
+++ scriptelegant.js
@@ -66,7 +66,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // --- Slideshow Setup ---
   const images = [
-    "img/ev.png", "img/img1.png", "img/img2.png", "img/img3.png", "img/img4.png",
+    "img/ev.png", "img/img1.jpg", "img/img2.png", "img/img3.png", "img/img4.png",
     "img/img5.png", "img/img6.png", "img/img7.png", "img/img8.png", "img/img9.png"
   ];
   const captions = [
